4fc35bbb6ea1e6ca9ceaabdf34e86c6808b28798
RohanDebnath <86291016+RohanDebnath@users.noreply.github.com>
2023-06-08 14:57:35 +0530

    Location Curd

--- app/src/main/java/com/example/toiletfinder/MainActivity.java
+++ app/src/main/java/com/example/toiletfinder/MainActivity.java
@@ -1,24 +1,19 @@
 package com.example.toiletfinder;
 
-import android.Manifest;
-import android.app.AlertDialog;
 import android.content.DialogInterface;
-import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
-import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.app.ActivityCompat;
-import androidx.core.content.ContextCompat;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
-import com.google.android.gms.maps.MapView;
 import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
@@ -28,31 +23,27 @@ import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
+import java.util.HashMap;
+
 public class MainActivity extends AppCompatActivity implements OnMapReadyCallback {
 
-    private static final int LOCATION_PERMISSION_REQUEST_CODE = 1;
-
-    private MapView mapView;
     private GoogleMap googleMap;
     private DatabaseReference databaseReference;
+    private HashMap<String, Marker> markerHashMap = new HashMap<>();
+    private HashMap<String, MarkerData> markerDataMap = new HashMap<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        mapView = findViewById(R.id.mapView);
-        mapView.onCreate(savedInstanceState);
-        mapView.getMapAsync(this);
-
+        // Initialize Firebase database reference
         databaseReference = FirebaseDatabase.getInstance().getReference("markers");
 
-        // Request location permission if not granted
-        if (ContextCompat.checkSelfPermission(this,
-                Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-            ActivityCompat.requestPermissions(this,
-                    new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
-                    LOCATION_PERMISSION_REQUEST_CODE);
+        // Obtain the SupportMapFragment and get notified when the map is ready to be used
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
+        if (mapFragment != null) {
+            mapFragment.getMapAsync(this);
         }
     }
 
@@ -68,6 +59,16 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
             }
         });
 
+        googleMap.setOnInfoWindowClickListener(new GoogleMap.OnInfoWindowClickListener() {
+            @Override
+            public void onInfoWindowClick(Marker marker) {
+                String markerId = (String) marker.getTag();
+                if (markerId != null) {
+                    showMarkerOptionsDialog(markerId);
+                }
+            }
+        });
+
         // Load existing markers from Firebase database
         databaseReference.addChildEventListener(new ChildEventListener() {
             @Override
@@ -78,18 +79,42 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
                             .position(new LatLng(markerData.getLatitude(), markerData.getLongitude()))
                             .title(markerData.getTitle())
                             .snippet(markerData.getDetails()));
-                    marker.setTag(dataSnapshot.getKey());
+                    marker.setTag(markerData.getMarkerId());
+
+                    // Store the MarkerData object in the HashMap
+                    markerHashMap.put(markerData.getMarkerId(), marker);
+                    markerDataMap.put(markerData.getMarkerId(), markerData);
                 }
             }
 
             @Override
             public void onChildChanged(@NonNull DataSnapshot dataSnapshot, @Nullable String previousChildName) {
-                // Handle the case when a child marker is updated
+                MarkerData markerData = dataSnapshot.getValue(MarkerData.class);
+                if (markerData != null) {
+                    String markerId = dataSnapshot.getKey();
+                    Marker marker = markerHashMap.get(markerId);
+                    if (marker != null) {
+                        marker.setTitle(markerData.getTitle());
+                        marker.setSnippet(markerData.getDetails());
+                        marker.showInfoWindow();
+
+                        // Update the MarkerData object in the HashMap
+                        markerDataMap.put(markerId, markerData);
+                    }
+                }
             }
 
             @Override
             public void onChildRemoved(@NonNull DataSnapshot dataSnapshot) {
-                // Handle the case when a child marker is removed
+                String markerId = dataSnapshot.getKey();
+                Marker marker = markerHashMap.get(markerId);
+                if (marker != null) {
+                    marker.remove();
+                    markerHashMap.remove(markerId);
+
+                    // Remove the MarkerData object from the HashMap
+                    markerDataMap.remove(markerId);
+                }
             }
 
             @Override
@@ -104,7 +129,6 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
         });
     }
 
-
     private void showMarkerInputDialog(final LatLng latLng) {
         AlertDialog.Builder builder = new AlertDialog.Builder(this);
         builder.setTitle("Add Marker");
@@ -116,21 +140,98 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
 
         builder.setPositiveButton("Add", new DialogInterface.OnClickListener() {
             @Override
+            public void onClick(DialogInterface dialog, int which) {
+                String title = titleEditText.getText().toString().trim();
+                String details = detailsEditText.getText().toString().trim();
+
+                // Generate a unique marker ID
+                String markerId = databaseReference.push().getKey();
+                if (markerId != null) {
+                    MarkerData markerData = new MarkerData(markerId, latLng.latitude, latLng.longitude, title, details);
+                    databaseReference.child(markerId).setValue(markerData);
+
+                    Marker marker = googleMap.addMarker(new MarkerOptions()
+                            .position(latLng)
+                            .title(title)
+                            .snippet(details));
+                    marker.setTag(markerId);
+
+                    // Store the MarkerData object in the HashMap
+                    markerHashMap.put(markerId, marker);
+                    markerDataMap.put(markerId, markerData);
+                }
+            }
+        });
+
+        builder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.cancel();
+            }
+        });
+
+        builder.show();
+    }
+
+    private void showMarkerOptionsDialog(final String markerId) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+        builder.setTitle("Marker Options");
+        builder.setItems(new CharSequence[]{"Edit Details", "Delete Marker"}, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                switch (which) {
+                    case 0: // Edit Details
+                        showEditMarkerDialog(markerId);
+                        break;
+                    case 1: // Delete Marker
+                        deleteMarker(markerId);
+                        break;
+                }
+            }
+        });
+        builder.show();
+    }
+
+
+    // ...
+
+    private void showEditMarkerDialog(final String markerId) {
+        // Retrieve the MarkerData object from the HashMap using the markerId
+        final MarkerData markerData = markerDataMap.get(markerId);
+        if (markerData == null) {
+            return;
+        }
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+        builder.setTitle("Edit Marker Details");
+        View view = getLayoutInflater().inflate(R.layout.dialog_marker_input, null);
+        builder.setView(view);
+
+        final EditText titleEditText = view.findViewById(R.id.titleEditText);
+        final EditText detailsEditText = view.findViewById(R.id.detailsEditText);
+
+        // Set the current marker details in the EditText fields
+        titleEditText.setText(markerData.getTitle());
+        detailsEditText.setText(markerData.getDetails());
+
+        builder.setPositiveButton("Save", new DialogInterface.OnClickListener() {
+            @Override
             public void onClick(DialogInterface dialogInterface, int i) {
                 String title = titleEditText.getText().toString().trim();
                 String details = detailsEditText.getText().toString().trim();
 
-                // Add marker to the map
-                Marker marker = googleMap.addMarker(new MarkerOptions()
-                        .position(latLng)
-                        .title(title)
-                        .snippet(details));
-                String markerKey = databaseReference.push().getKey();
-                marker.setTag(markerKey);
+                // Update the marker details in the HashMap and Firebase
+                markerData.setTitle(title);
+                markerData.setDetails(details);
+                databaseReference.child(markerId).setValue(markerData);
 
-                // Save marker data to Firebase database
-                MarkerData markerData = new MarkerData(latLng.latitude, latLng.longitude, title, details);
-                databaseReference.child(markerKey).setValue(markerData);
+                // Update the marker's info window with the new details
+                Marker marker = markerHashMap.get(markerId);
+                if (marker != null) {
+                    marker.setTitle(title);
+                    marker.setSnippet(details);
+                    marker.showInfoWindow();
+                }
             }
         });
 
@@ -144,51 +245,21 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
         builder.show();
     }
 
-    @Override
-    protected void onStart() {
-        super.onStart();
-        mapView.onStart();
-    }
+// ...
 
-    @Override
-    protected void onResume() {
-        super.onResume();
-        mapView.onResume();
-    }
 
-    @Override
-    protected void onPause() {
-        super.onPause();
-        mapView.onPause();
-    }
 
-    @Override
-    protected void onStop() {
-        super.onStop();
-        mapView.onStop();
-    }
 
-    @Override
-    protected void onDestroy() {
-        super.onDestroy();
-        mapView.onDestroy();
-    }
-
-    @Override
-    public void onLowMemory() {
-        super.onLowMemory();
-        mapView.onLowMemory();
-    }
-
-    @Override
-    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
-        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
-        if (requestCode == LOCATION_PERMISSION_REQUEST_CODE) {
-            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-                googleMap.setMyLocationEnabled(true);
-            } else {
-                Toast.makeText(this, "Location permission denied", Toast.LENGTH_SHORT).show();
-            }
+    private void deleteMarker(final String markerId) {
+        // Remove the marker from the map
+        Marker marker = markerHashMap.get(markerId);
+        if (marker != null) {
+            marker.remove();
+            markerHashMap.remove(markerId);
         }
+
+        // Remove the marker data from the HashMap and Firebase
+        markerDataMap.remove(markerId);
+        databaseReference.child(markerId).removeValue();
     }
 }
--- app/src/main/java/com/example/toiletfinder/MarkerData.java
+++ app/src/main/java/com/example/toiletfinder/MarkerData.java
@@ -1,6 +1,7 @@
 package com.example.toiletfinder;
 public class MarkerData {
 
+    private String markerId; // Unique identifier for the marker
     private double latitude;
     private double longitude;
     private String title;
@@ -10,11 +11,36 @@ public class MarkerData {
         // Default constructor required for Firebase
     }
 
-    public MarkerData(double latitude, double longitude, String title, String details) {
+    public void setMarkerId(String markerId) {
+        this.markerId = markerId;
+    }
+
+    public void setLatitude(double latitude) {
+        this.latitude = latitude;
+    }
+
+    public void setLongitude(double longitude) {
+        this.longitude = longitude;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public void setDetails(String details) {
+        this.details = details;
+    }
+
+    public MarkerData(String markerId, double latitude, double longitude, String title, String details) {
+        this.markerId = markerId;
         this.latitude = latitude;
         this.longitude = longitude;
         this.title = title;
         this.details = details;
+    }
+
+    public String getMarkerId() {
+        return markerId;
     }
 
     public double getLatitude() {
@@ -33,4 +59,5 @@ public class MarkerData {
         return details;
     }
 }
+
 
